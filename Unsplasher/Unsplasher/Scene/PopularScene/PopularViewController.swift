a9c51f975dba334a2bc472fcc1698ef2748d1a95
kyungpyoda <qorghdl@naver.com>
2021-06-14 20:54:14 +0900

    [chore] Add Scene group

---
+++ Unsplasher/Unsplasher/Scene/PopularScene/PopularViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PopularViewController.swift
+//  Unsplasher
+//
+//  Created by 홍경표 on 2021/06/14.
+//
+
+import UIKit
+
+final class PopularViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUp()
+    }
+    
+    private func setUp() {
+        
+    }
+    
+}
