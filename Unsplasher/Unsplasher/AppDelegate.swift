88457d84da43ae2af9ebfe993233c747ec31cc1d
kyungpyoda <qorghdl@naver.com>
2021-06-14 20:34:03 +0900

    Init Project
    - iOS 13.0+
    - without SceneDelegate
    - implement project without Storyboard

---
+++ Unsplasher/Unsplasher/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Unsplasher
+//
+//  Created by 홍경표 on 2021/06/14.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        return true
+    }
+    
+}
