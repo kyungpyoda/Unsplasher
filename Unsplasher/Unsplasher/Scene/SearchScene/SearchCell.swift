1aaf4a8cdf76990b998a45ab2df9f07449b3abc4
kyungpyoda <qorghdl@naver.com>
2021-06-22 00:47:28 +0900

    [feat] Implement SearchCell UI

---
+++ Unsplasher/Unsplasher/Scene/SearchScene/SearchCell.swift
@@ -0,0 +1,52 @@
+//
+//  SearchCell.swift
+//  Unsplasher
+//
+//  Created by 홍경표 on 2021/06/21.
+//
+
+import UIKit
+
+final class SearchCell: UICollectionViewCell {
+    
+    static let defaultImage: UIImage? = .init(systemName: "photo")?.withRenderingMode(.alwaysOriginal).withTintColor(.systemGray3)
+    
+    private let imageView: UIImageView = .init().then {
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUp() {
+        contentView.backgroundColor = .systemGray5
+        
+        contentView.layer.cornerRadius = 15
+        contentView.layer.masksToBounds = true
+        
+        layer.shadowColor = UIColor.label.cgColor
+        layer.shadowOffset = .zero
+        layer.shadowOpacity = 0.5
+        layer.shadowRadius = 5
+        
+        contentView.addSubview(imageView)
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        imageView.image = Self.defaultImage
+    }
+    
+    private func configure(imageURLStr: String) {
+        // TODO: implement image loader, fetch image with url
+    }
+    
+    override func prepareForReuse() {
+        self.imageView.image = Self.defaultImage
+    }
+}
