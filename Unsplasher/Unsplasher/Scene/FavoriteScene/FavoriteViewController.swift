8fa9e24f81cce39e6459bd74718bf9d5e3af1b9d
kyungpyoda <qorghdl@naver.com>
2021-06-27 17:24:00 +0900

    [feat] Implement FavoriteCell UI

--- Unsplasher/Unsplasher/Scene/FavoriteScene/FavoriteViewController.swift
+++ Unsplasher/Unsplasher/Scene/FavoriteScene/FavoriteViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 final class FavoriteViewController: UIViewController {
     
+    private let collectionView: UICollectionView = .init(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
+        $0.backgroundColor = .systemBackground
+    }
+    
     init() {
         super.init(nibName: nil, bundle: nil)
     }
@@ -22,7 +26,49 @@ final class FavoriteViewController: UIViewController {
     }
     
     private func setUp() {
-        view.backgroundColor = .systemOrange
+        setUpUI()
+        setUpCollectionView()
+    }
+    
+    private func setUpUI() {
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints {
+            $0.edges.equalTo(self.view.safeAreaLayoutGuide)
+        }
+    }
+    
+    private func setUpCollectionView() {
+        collectionView.register(FavoriteCell.self, forCellWithReuseIdentifier: FavoriteCell.reuseIdentifier)
+        collectionView.dataSource = self
+        
+        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            let itemSize = view.bounds.width / 2 * 0.8
+            let spacing = (view.bounds.width - itemSize * 2) / 3
+            flowLayout.itemSize = .init(width: itemSize, height: itemSize)
+            flowLayout.sectionInset = .init(top: spacing / 2, left: spacing, bottom: spacing / 2, right: spacing)
+            flowLayout.minimumLineSpacing = spacing
+            flowLayout.minimumInteritemSpacing = 0
+        }
+    }
+    
+}
+
+
+// MARK: - CollectionView DataSource
+
+extension FavoriteViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FavoriteCell.reuseIdentifier, for: indexPath) as? FavoriteCell else {
+            return UICollectionViewCell()
+        }
+        return cell
     }
     
 }
